c9e3c225be1f09865f54ef2658d04ea81e7f858c
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2019-09-22 19:04:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# useNotification
+
+## The simplest way to add a notification system to your React app 🛎️
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function useNotification() {
+  return <div>Hello notification</div>
+}
